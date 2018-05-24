1b2c32999f1ac02d7f7473df049cbf7b2098180d
hypersonicgx <32492561+hypersonicgx@users.noreply.github.com>
2018-05-24 11:29:44 -0400

    Add files via upload

---
+++ types.php
@@ -0,0 +1,32 @@
+<?php
+using namespace std;
+/*
+type damage program
+*/
+
+int main() {
+   var fairy = "fairy";
+   var dragon = "dragon";
+   var water = "water";
+   var fire = "fire";
+   var electric = "electric";
+   var grass = "grass";
+   var ground = "ground";
+   var ice = "ice";
+   var dark = "dark";
+   var rock = "rock";
+   var fighting = "fighting"
+   var flying = "flying";
+   var normal = "normal";
+   var ghost = "ghost";
+   var steel = "steel";
+   var psychic = "psychic";
+   var poison = "posion";
+   var bug = "bug";
+   int damage = .10;
+   int Seffective = .20;
+   int Neffective = .05;
+   cout << "Super effective";
+   return 0;
+}
+?>
